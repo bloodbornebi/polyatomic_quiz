9cf20c94b1e6468682fa75da25ac558d347ef627
bloodbornebi <40738672+bloodbornebi@users.noreply.github.com>
2018-11-09 14:17:31 -0500

    Fixed minor formatting bug

--- script.js
+++ script.js
@@ -16,13 +16,11 @@ function showButtons() {
 
 function pai() {
  // TODO: Fix charge type output
- // TODO: Finish adding ions
- var ions = [["Ammonium", "NH4+"],["Hydronium", "H3O+"],["Nitrate", "NO3-"], ["Nitrite", "NO2-"], ["Hydroxide", "OH-"], ["Permanganate", "MnO4-"], ["Hydrogen carbonate", "HCO3-"], ["Perchlorate", "ClO4-"], ["Chlorate", "ClO3-"], ["Chlorite", "ClO2-"], ["Hypochlorite", "ClO-"], ["Cyanide", "CN-"], ["Acetate", "C2H3O2-"], ["Thiocynate", "SCN-"], ["Dihydrogen phosphate", "H2PO4-"], ["Hydrogen sulfate", "HSO4-"], ["Perbromate", "BrO4-"], ["Bromate", "BrO3-"], ["Bromite", "BrO2-"], ["Hypobromite", "BrO-"], ["Periodate", "IO4-"], ["Iodate", "IO3-"], ["Hypoiodite", "IO-"], ["Cyanate", "OCN-"], ["Carbonate", "CO3 2-"], ["Dichromate", "Cr2O7 2-"], ["Thiosulfate", "S2O3 2-"], ["Chromate", "CrO4 2-"], ["Hydrogen phosphate", "H2PO4 2-"], ["Oxalate", "C2O4 2-"], ["Sulfate", "SO4 2-"], ["Sulfite", "SO3 2-"], ["Peroxide", "O2 2-"], ["Tartrate", "C4O4O6 2-"], ["Tetraborate", "Br4O7 2-"], ["Hexafluorosilicate", "SiF6 2-"], ["Phosphate", "PO4 3-"], ["Arsenate", "AsO4 3-"], ["Borate", "BO3 3-"], ["Phosphite", "PO3 3-"], ["Hydrogen peroxide", "H2O2"], ["Ammonia", "NH3"], ["Methane", "CH4"], ["Propane", "C3H8"], ["C2H5OH", "Ethanol"], ["HCN", "Hydrocyannic acid"], ["Glucose", "C6H12O6"], ["Nitric oxide", "NO"], ["Phosphine", "PH3"]];
+ var ions = [["Ammonium", "NH4+"],["Hydronium", "H3O+"],["Nitrate", "NO3-"], ["Nitrite", "NO2-"], ["Hydroxide", "OH-"], ["Permanganate", "MnO4-"], ["Hydrogen carbonate", "HCO3-"], ["Perchlorate", "ClO4-"], ["Chlorate", "ClO3-"], ["Chlorite", "ClO2-"], ["Hypochlorite", "ClO-"], ["Cyanide", "CN-"], ["Acetate", "C2H3O2-"], ["Thiocynate", "SCN-"], ["Dihydrogen phosphate", "H2PO4-"], ["Hydrogen sulfate", "HSO4-"], ["Perbromate", "BrO4-"], ["Bromate", "BrO3-"], ["Bromite", "BrO2-"], ["Hypobromite", "BrO-"], ["Periodate", "IO4-"], ["Iodate", "IO3-"], ["Hypoiodite", "IO-"], ["Cyanate", "OCN-"], ["Carbonate", "CO3 2-"], ["Dichromate", "Cr2O7 2-"], ["Thiosulfate", "S2O3 2-"], ["Chromate", "CrO4 2-"], ["Hydrogen phosphate", "H2PO4 2-"], ["Oxalate", "C2O4 2-"], ["Sulfate", "SO4 2-"], ["Sulfite", "SO3 2-"], ["Peroxide", "O2 2-"], ["Tartrate", "C4O4O6 2-"], ["Tetraborate", "Br4O7 2-"], ["Hexafluorosilicate", "SiF6 2-"], ["Phosphate", "PO4 3-"], ["Arsenate", "AsO4 3-"], ["Borate", "BO3 3-"], ["Phosphite", "PO3 3-"], ["Hydrogen peroxide", "H2O2"], ["Ammonia", "NH3"], ["Methane", "CH4"], ["Propane", "C3H8"], ["Ethanol", "C2H5OH"], ["Hydrocyannic acid", "HCN"], ["Glucose", "C6H12O6"], ["Nitric oxide", "NO"], ["Phosphine", "PH3"]];
  // TODO: find way to make tab output
  var quiz = "Question &emsp;&emsp; Answer <br>";
 
  // TODO: Can i generalize this particular loop because I call it twice
- // TODO: This output is ugly AF
  // TODO: Check for duplicates
  // TODO: Append quiz on to the end of the log, reset clears the area
  // TODO: Add ions to names
