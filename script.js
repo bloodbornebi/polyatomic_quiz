80f9ff6111ee64eba50e102cc42d3b62766bbe0b
bloodbornebi <40738672+bloodbornebi@users.noreply.github.com>
2018-11-10 10:42:48 -0500

    testing generate function

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ function reset() {
 };
 
 function generate(content) {
+	var test = document.getElementById("test");
+	test.innerHTML = "Made it";
  var quiza = document.getElementById("quiz");
  quiza.innerHTML = content;
 };
@@ -13,8 +15,6 @@ function showButtons() {
 };
 
 function pai() {
-	var test = document.getElementById("test");
-	test.innerHTML = "Made it";
 	var ions = [["Test one", "T1"], ["Test two", "T2"]];
  // var ions = [["Ammonium", "NH4 <sup>+</sup>"],["Hydronium", "H<sub>3</sub>O <sup>+</sup>"],["Nitrate", "NO<sub>3</sub> <sup>-</sup>"], ["Nitrite", "NO2 <sup>-</sup>"], ["Hydroxide", "OH <sup>-</sup>"], ["Permanganate", "MnO4 <sup>-</sup>"], ["Hydrogen carbonate", "HCO<sub>3</sub> <sup>-</sup>"], ["Perchlorate", "ClO4 <sup>-</sup>"], ["Chlorate", "ClO<sub>3</sub> <sup>-</sup>"], ["Chlorite", "ClO2 <sup>-</sup>"], ["Hypochlorite", "ClO <sup>-</sup>"], ["Cyanide", "CN <sup>-</sup>"], ["Acetate", "C2H<sub>3</sub>O2 <sup>-</sup>"], ["Thiocynate", "SCN <sup>-</sup>"], ["Dihydrogen phosphate", "H2PO4 <sup>-</sup>"], ["Hydrogen sulfate", "HSO4 <sup>-</sup>"], ["Perbromate", "BrO4 <sup>-</sup>"], ["Bromate", "BrO<sub>3</sub> <sup>-</sup>"], ["Bromite", "BrO2 <sup>-</sup>"], ["Hypobromite", "BrO <sup>-</sup>"], ["Periodate", "IO4 <sup>-</sup>"], ["Iodate", "IO<sub>3</sub> <sup>-</sup>"], ["Hypoiodite", "IO <sup>-</sup>"], ["Cyanate", "OCN <sup>-</sup>"], ["Carbonate", "CO<sub>3</sub> 2-"], ["Dichromate", "Cr2O7 2-"], ["Thiosulfate", "S2O<sub>3</sub> 2-"], ["Chromate", "CrO4 2-"], ["Hydrogen phosphate", "H2PO4 2-"], ["Oxalate", "C2O4 2-"], ["Sulfate", "SO4 2-"], ["Sulfite", "SO<sub>3</sub> 2-"], ["Peroxide", "O2 2-"], ["Tartrate", "C4O4O6 2-"], ["Tetraborate", "Br4O7 2-"], ["Hexafluorosilicate", "SiF6 2-"], ["Phosphate", "PO4 <sub>3</sub>-"], ["Arsenate", "AsO4 <sub>3</sub>-"], ["Borate", "BO<sub>3</sub> <sub>3</sub>-"], ["Phosphite", "PO<sub>3</sub> <sub>3</sub>-"], ["Hydrogen peroxide", "H2O2"], ["Ammonia", "NH<sub>3</sub>"], ["Methane", "CH4"], ["Propane", "C<sub>3</sub>H8"], ["Ethanol", "C2H5OH"], ["Hydrocyannic acid", "HCN"], ["Glucose", "C6H12O6"], ["Nitric oxide", "NO"], ["Phosphine", "PH<sub>3</sub>"]];
 
